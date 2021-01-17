a70d811aa8054e4e6216f96e5e19de6da8e4b2ec
hizukayudai@gmail.com <09120912Abcd>
2021-01-17 20:44:12 +0900

    move domain to usecase

--- domain/user.go
+++ domain/user.go
@@ -1,28 +1,13 @@
 package domain
 
-import (
-	"fmt"
-
-	"github.com/jinzhu/gorm"
-	"golang.org/x/crypto/bcrypt"
-)
+import "time"
 
 type User struct {
-	gorm.Model
-	Name     string
-	Email    string
-	Password string
-}
-
-func (user *User) SetPassword() error {
-	hash, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
-	if err != nil {
-		return fmt.Errorf("failed to set password: %w", err)
-	}
-	user.Password = string(hash)
-	return nil
-}
-
-func (user *User) PasswordVerify(password string) error {
-	return bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password))
+	ID                int
+	Name              string
+	Email             string
+	Password          string `gorm:"-"`
+	EncryptedPassword string
+	CreatedAt         time.Time
+	UpdatedAt         time.Time
 }
--- usecase/interactor/user_interactor.go
+++ usecase/interactor/user_interactor.go
@@ -3,7 +3,24 @@ package interactor
 import (
 	"app/domain"
 	"app/usecase/repository"
+
+	"golang.org/x/crypto/bcrypt"
 )
+
+func genEncryptedPassword(password string) (string, error) {
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+	return string(hash), nil
+}
+
+func compareHashAndPass(encryptedPassword string, password string) error {
+	if err := bcrypt.CompareHashAndPassword([]byte(encryptedPassword), []byte(password)); err != nil {
+		return err
+	}
+	return nil
+}
 
 type UserInteractor struct {
 	UserRepository repository.UserRepository
@@ -18,7 +35,12 @@ func (u *UserInteractor) GetByID(id int) (*domain.User, error) {
 }
 
 func (u *UserInteractor) Create(user *domain.User) error {
-	err := u.UserRepository.Create(user)
+	encryptedPassword, err := genEncryptedPassword(user.Password)
+	if err != nil {
+		return err
+	}
+	user.EncryptedPassword = encryptedPassword
+	err = u.UserRepository.Create(user)
 	if err != nil {
 		return err
 	}
