bc2caae4f90a13fb8b6be8ccdf7718c6d168ce5f
hizukayudai@gmail.com <09120912Abcd>
2021-01-17 20:43:42 +0900

    fix err handling

--- interfaces/repository/user_repository.go
+++ interfaces/repository/user_repository.go
@@ -2,7 +2,6 @@ package repository
 
 import (
 	"app/domain"
-	"errors"
 	"fmt"
 
 	"github.com/jinzhu/gorm"
@@ -16,7 +15,7 @@ func (u *UserRepository) GetByID(id int) (*domain.User, error) {
 	var user domain.User
 	if err := u.DB.Take(&user, id).Error; err != nil {
 		if gorm.IsRecordNotFoundError(err) {
-			err = errors.New("not found")
+			err = err
 		}
 		return nil, err
 	}
@@ -24,12 +23,8 @@ func (u *UserRepository) GetByID(id int) (*domain.User, error) {
 }
 
 func (u *UserRepository) Create(user *domain.User) error {
-	err := user.SetPassword()
-	if err != nil {
-		return err
-	}
 	result := u.DB.Create(user)
-	if err = result.Error; err != nil {
+	if err := result.Error; err != nil {
 		return fmt.Errorf("failed to create user: %w", err)
 	}
 	return nil
