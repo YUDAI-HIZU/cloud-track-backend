a50a8a3ca79b04370647068a65035e071c3b96e8
hizukayudai@gmail.com <09120912Abcd>
2021-01-24 01:59:44 +0900

    add profiles

--- infrastructure/router.go
+++ infrastructure/router.go
@@ -28,19 +28,36 @@ func NewRouter(db *gorm.DB) *Router {
 
 func (r *Router) setRouter() {
 	userController := controllers.NewUserController(r.DB)
-	r.Gin.GET("/users/:id", func(c *gin.Context) {
-		userController.GetByID(c)
-	})
+
 	r.Gin.POST("/sign-up", func(c *gin.Context) {
 		userController.Create(c)
 	})
 	r.Gin.POST("sign-in", func(c *gin.Context) {
 		userController.SignIn(c)
 	})
-	r.Gin.GET("/auth", middleware.AuthMiddleware())
-	r.Gin.GET("/account", middleware.AuthMiddleware(), func(c *gin.Context) {
-		userController.CurrentUser(c)
-	})
+
+	users := r.Gin.Group("/users")
+	{
+		users.GET("/:id", func(c *gin.Context) {
+			userController.GetByID(c)
+		})
+	}
+
+	account := r.Gin.Group("/account")
+	{
+		account.GET("/", middleware.AuthMiddleware(), func(c *gin.Context) {
+			userController.CurrentUser(c)
+		})
+		account.GET("/profiles", middleware.AuthMiddleware(), func(c *gin.Context) {
+
+		})
+		account.POST("/profiles", middleware.AuthMiddleware(), func(c *gin.Context) {
+
+		})
+		account.PUT("/profiles", middleware.AuthMiddleware(), func(c *gin.Context) {
+
+		})
+	}
 }
 
 func (r *Router) Run() {
